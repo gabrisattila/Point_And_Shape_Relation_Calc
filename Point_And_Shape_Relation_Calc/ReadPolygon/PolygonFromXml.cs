240e785428accb5aeab8ec61c9e5da9ac2f6874a
Gábris Attila <gabris.attila.hun@gmail.com>
2024-02-20 20:45:10 +0100

    File reading to shape is ready

---
+++ Point_And_Shape_Relation_Calc/ReadPolygon/PolygonFromXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Point_And_Shape_Relation_Calc.ReadPolygon.Read;
+
+namespace Point_And_Shape_Relation_Calc.ReadPolygon
+{
+    public class PolygonFromXml
+    {
+        private String path;
+
+        private Polygon.Polygon polygon;
+
+        public String Path { get { return path; } set {  path = value; } }
+
+        public Polygon.Polygon Polygon { get { return polygon; } private set { polygon = value; } }
+
+        public PolygonFromXml(String path)
+        {
+            Path = path;
+            createPolygon();
+        }
+
+        private void createPolygon()
+        {
+            ReadXml xmlReader = new ReadXml(Path);
+            Polygon = new Polygon.Polygon(xmlReader.Points);
+        }
+    }
+}
---
+++ Point_And_Shape_Relation_Calc/ReadPolygon/Read/ReadXml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Point_And_Shape_Relation_Calc.ReadPolygon.Polygon;
+
+namespace Point_And_Shape_Relation_Calc.ReadPolygon.Read
+{
+    public class ReadXml
+    {
+        private String path;
+
+        private List<Point> points;
+
+        public String Path { get { return path; } set { path = value; } }
+
+        public List<Point> Points { get { return points; } }
+
+        public ReadXml(String path)
+        {
+            Path = path;
+        }
+
+        private void fillPointListBase()
+        {
+            points = new List<Point>();
+
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(Path);
+
+            XmlNode root = doc.DocumentElement;
+
+            XmlNodeList nodeList = root.SelectNodes("//Point");
+
+            foreach (XmlNode node in nodeList)
+            {
+                String x = node.SelectSingleNode("X").InnerText;
+
+                String y = node.SelectSingleNode("Y").InnerText;
+
+                int X = Convert.ToInt32(x), Y = Convert.ToInt32(y);
+
+                points.Add(new Point(X, Y));
+            }
+        }
+
+
+    }
+}
